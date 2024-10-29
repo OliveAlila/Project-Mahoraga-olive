856f17c38beeaf52b720f4c2713f37e2840382a3
Karume-lab <danielkarume@outlook.com>
2024-10-29 23:49:53 +0300

    feat: :sparkles: introduction to C

---
+++ C/1.overview/arrays.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char numArray[20] = {
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+    };
+
+    printf("%c", numArray[0]);
+    printf("%c", numArray[1]);
+    printf("%c", numArray[2]);
+    printf("%c", numArray[3]);
+    printf("%c", numArray[4]);
+
+    return 0;
+
+}
---
+++ C/1.overview/notes.md
@@ -0,0 +1,21 @@
+1. Data types
+2. Variables
+3. Functions
+   1. Printing a string  
+4. Arrays
+5. Structs
+6. Pointers 
+7. Header files
+
+
+TO DO:
+1. main args
+2. header files
+
+
+
+ASSIGNMENTS:
+1. read on preprocessors
+2. float vs double
+3. read on datatypes
+4. formal definition of an array
